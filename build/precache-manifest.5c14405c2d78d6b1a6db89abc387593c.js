f7711129dc363d8cbb47cda3f3573e77a2945e7d
asit-prakash <asit.prakash@innoraft.com>
2020-12-08 17:59:46 +0530

    added production build configs

---
+++ build/precache-manifest.5c14405c2d78d6b1a6db89abc387593c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34dd5578a2ceed162194ad94197ec2ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a12318260b0bccdda81d",
+    "url": "/static/css/main.4e3614f9.chunk.css"
+  },
+  {
+    "revision": "a809be22474244997922",
+    "url": "/static/js/2.ee7efde7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ee7efde7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a12318260b0bccdda81d",
+    "url": "/static/js/main.be2d9b5d.chunk.js"
+  },
+  {
+    "revision": "8bc023a6f72c915a98db",
+    "url": "/static/js/runtime-main.c959de8f.js"
+  },
+  {
+    "revision": "c2836c9b00be76f50e4e90e3a2352971",
+    "url": "/static/media/Cheese.c2836c9b.jpeg"
+  },
+  {
+    "revision": "eb30d83e28d1f3676ba604c9c235ed97",
+    "url": "/static/media/Chicken.eb30d83e.jpeg"
+  },
+  {
+    "revision": "1494678fe0254521bc5d08f94e5e5a90",
+    "url": "/static/media/Mayonnaise.1494678f.jpeg"
+  },
+  {
+    "revision": "91fdfe664c54058675573484ac0451f3",
+    "url": "/static/media/Salad.91fdfe66.jpeg"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
